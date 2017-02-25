0c959e78408bdb886c9193781f0d4772d5a61117
bgortych <bartlomiejgortych@gmail.com>
2017-02-25 20:25:19 +0100

    Przyrost 1 - fragmenty, egran główny gui

--- app/src/main/java/pl/bgortych/simpletumblrclient/MainActivity.java
+++ app/src/main/java/pl/bgortych/simpletumblrclient/MainActivity.java
@@ -1,14 +1,23 @@
 package pl.bgortych.simpletumblrclient;
 
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import pl.bgortych.simpletumblrclient.searcher.UserSearchFragment;
+
+public class MainActivity extends FragmentActivity implements UserSearchFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Git test
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
     }
 }
