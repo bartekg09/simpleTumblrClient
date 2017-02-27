ec7e327350afd6e6e6c199dc4b56d41039b08471
bgortych <bartlomiejgortych@gmail.com>
2017-02-27 20:02:14 +0100

    przyrost 2

---
+++ app/src/main/java/pl/bgortych/simpletumblrclient/TumblrHttpClient.java
@@ -0,0 +1,8 @@
+package pl.bgortych.simpletumblrclient;
+
+/**
+ * Created by bgortych on 2017-02-26.
+ */
+
+public class TumblrHttpClient {
+}
---
+++ app/src/main/java/pl/bgortych/simpletumblrclient/model/SinglePost.java
@@ -0,0 +1,8 @@
+package pl.bgortych.simpletumblrclient.model;
+
+/**
+ * Created by bgortych on 2017-02-26.
+ */
+
+public class SinglePost {
+}
